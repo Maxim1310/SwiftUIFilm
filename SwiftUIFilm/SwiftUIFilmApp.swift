d40e795c7583972bae874f85f91d7d5421812be8
Maxim1310 <makimr@internet.ru>
2026-03-12 19:53:50 +0200

    Initial Commit

---
+++ SwiftUIFilm/SwiftUIFilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFilmApp.swift
+//  SwiftUIFilm
+//
+//  Created by Maxik on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
